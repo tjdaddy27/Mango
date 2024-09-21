77123033333f6daf3048411ef34aac1ccd79120b
karbinMohammed <karbindabin@gmail.com>
2024-09-21 17:38:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeText() {
+    // Change the text when the button is clicked
+    document.getElementById('text').innerText = "You Are The Best Father In The World, But With Many Rules, Haha - Karbin";
+}
